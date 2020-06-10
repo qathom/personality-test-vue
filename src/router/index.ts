2713b0729db947a75814588a0569b390bb5730df
qathom <anthony.me@protonmail.ch>
2020-06-11 00:21:03 +0200

    Updated path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import Home from '@/views/Home.vue';
-import Test from '@/views/Test.vue';
+import ProfileTest from '@/views/ProfileTest.vue';
 
 Vue.use(VueRouter);
 
@@ -14,7 +14,7 @@ const routes: Array<RouteConfig> = [
   {
     path: '/test',
     name: 'Test',
-    component: Test,
+    component: ProfileTest,
   },
 ];
 
