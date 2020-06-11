b853fba030ea56d30ebb1f183c2cb502b9d38706
qathom <anthony.me@protonmail.ch>
2020-06-11 17:33:19 +0200

    Use versioning component in home page

---
+++ src/components/Versioning.vue
@@ -0,0 +1,18 @@
+<template>
+   <p>
+    <small>v{{version}}</small>
+  </p>
+</template>
+<script lang='ts'>
+import {
+  Component,
+  Prop,
+  Vue,
+} from 'vue-property-decorator';
+
+@Component({})
+export default class Versioning extends Vue {
+  @Prop(String)
+  readonly version!: string;
+}
+</script>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -4,5 +4,38 @@
     <p class="lead font-weight-normal">What is your dominant color?</p>
     <hr />
     <b-button to="/test" variant="primary">Start the test</b-button>
+
+    <div class="mt-5">
+      <versioning :version=version />
+    </div>
   </div>
 </template>
+
+<script lang='ts'>
+import {
+  Vue,
+  Component,
+  Prop,
+} from 'vue-property-decorator';
+import { Doughnut, mixins } from 'vue-chartjs';
+import { ChartData, ChartOptions } from 'chart.js';
+import Versioning from '@/components/Versioning.vue';
+import { version } from '../../package.json';
+
+@Component({
+  extends: Doughnut,
+  mixins: [mixins.reactiveProp],
+  components: {
+    Versioning,
+  },
+})
+export default class Welcome extends Vue {
+  @Prop(Object)
+  readonly chartData!: ChartData;
+
+  @Prop(Object)
+  readonly options!: ChartOptions;
+
+  private version = version;
+}
+</script>
