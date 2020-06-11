6468b9053bad8494340d1d11a194a59bfa5853c6
qathom <anthony.me@protonmail.ch>
2020-06-11 22:01:46 +0200

    Use hash based routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,7 +19,7 @@ const routes: Array<RouteConfig> = [
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   linkActiveClass: 'active',
   routes,
