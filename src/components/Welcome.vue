ebc6cec123e2eb82ccd9ed4196c75865f4e30202
qathom <anthony.me@protonmail.ch>
2020-06-11 16:51:47 +0200

    Changed font weight

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="welcome">
     <h1>Personality test</h1>
-    <p class="lead">What is your dominant color?</p>
+    <p class="lead font-weight-normal">What is your dominant color?</p>
     <hr />
     <b-button to="/test" variant="primary">Start the test</b-button>
   </div>
